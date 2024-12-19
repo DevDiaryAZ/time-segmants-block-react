074f3338e4b52765898aa16667550e28e0641f54
Alena Zarubina <alenazarubi@gmail.com>
2024-12-20 00:15:09 +0300

    delete files

--- src/utils/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const formatNumber = (number: number): string => {
-  return number < 10 ? '0' + number : number.toString();
-};
-
-export const getStyleNumber = (style: string): number => {
-  return Number(style.replace('px', ''));
-};
